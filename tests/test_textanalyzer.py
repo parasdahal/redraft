50b768c2b684669a0d7a89e5a8bdb7de133eb2f5
Paras Dahal <shree5paras@gmail.com>
2017-01-03 16:45:51 +0530

    Added setuUp method to test_textanalyzer class

--- tests/test_textanalyzer.py
+++ tests/test_textanalyzer.py
@@ -1,16 +1,20 @@
 import os.path, sys
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir))
 
+from textanalyzer import TextAnalyzer
 import unittest
 class TextAnalyzerTest(unittest.TestCase):
     """
     Test the methods of TextAnalyzer class
     """
+    def setUp(self):
+        analyzer = TextAnalyzer()
+
     def test_parse(self):
         """
         Test the stats dict returned by the parse method
         """
-        self.assertEqual(1,1)
+        self.assertEqual(1,1,"Demo test")
 
 if __name__ == "__main__":
     unittest.main()
