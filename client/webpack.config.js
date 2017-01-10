3c3f7c2004c497b12bd723d47996f35648da3653
Paras Dahal <shree5paras@gmail.com>
2017-01-10 17:34:58 +0530

    Reorganized directory and added a gitignore for app directory

---
+++ client/webpack.config.js
@@ -0,0 +1,24 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: __dirname,
+    filename: 'bundle.js',
+    publicPath: './src/assets/'
+  },
+  module: {
+    loaders: [
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        include: path.join(__dirname, 'src'),
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+};
